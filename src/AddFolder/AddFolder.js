5774166fb05391eee2985bb105bb157e2bc86267
Xiaolei Wang <xwang@sandiego.edu>
2019-10-21 20:58:28 -0700

    added validation to AddFolder and AddNote

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
 import ApiContext from '../ApiContext'
+import ValidationError from '../ValidationError/ValidationError'
 import config from '../config'
 import './AddFolder.css'
 
@@ -10,6 +11,45 @@ export default class AddFolder extends Component {
       push: () => { }
     },
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      folderName: '',
+      folderNameValid: false,
+      formValid: false,
+      validationMessages: {
+        folderTitle: ''
+      }
+    }
+  }
+
+  updateFolderName(folderName) {
+    this.setState({folderName}, () => {this.validateFolderName(folderName)});
+  }
+
+  validateFolderName(fieldValue) {
+    const fieldErrors = {...this.state.validationMessages};
+    let hasError = false;
+
+    fieldValue = fieldValue.trim();
+    if(fieldValue.length === 0) {
+      fieldErrors.folderTitle = 'Please type a Name for this folder';
+      hasError = true;
+    }
+
+    this.setState({
+      validationMessages: fieldErrors,
+      folderNameValid: !hasError
+    }, this.formValid);
+  } 
+
+  formValid() {
+    this.setState({
+      formValid: this.state.folderNameValid
+    });
+  }
+
   static contextType = ApiContext;
 
   handleSubmit = e => {
@@ -47,10 +87,11 @@ export default class AddFolder extends Component {
             <label htmlFor='folder-name-input'>
               Name
             </label>
-            <input type='text' id='folder-name-input' name='folder-name' />
+            <input type='text' id='folder-name-input' name='folder-name' onChange={e => this.updateFolderName(e.target.value)} />
+            <ValidationError hasError={!this.state.folderNameValid} message={this.state.validationMessages.folderTitle} /> 
           </div>
           <div className='buttons'>
-            <button type='submit'>
+            <button type='submit' disabled={!this.state.formValid}>
               Add folder
             </button>
           </div>
